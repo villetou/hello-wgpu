c752e57641cf6804e548ec78a01ebda6b470447f
Ville Touronen <touronen@gmail.com>
2020-12-23 19:17:31 +0200

    Remove opengl transformation matrix from rendering.rs

--- src/rendering.rs
+++ src/rendering.rs
@@ -15,14 +15,6 @@ struct Vertex {
     tex_coords: [f32; 2],
 }
 
-#[rustfmt::skip]
-pub const OPENGL_TO_WGPU_MATRIX: cgmath::Matrix4<f32> = cgmath::Matrix4::new(
-    1.0, 0.0, 0.0, 0.0,
-    0.0, 1.0, 0.0, 0.0,
-    0.0, 0.0, 0.5, 0.0,
-    0.0, 0.0, 0.5, 1.0,
-);
-
 #[repr(C)]
 // This is so we can store this in a buffer
 #[derive(Debug, Copy, Clone, bytemuck::Pod, bytemuck::Zeroable)]
