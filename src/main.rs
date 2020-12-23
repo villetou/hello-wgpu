f390d0633c07c95f9bc8863d85e4b22de6dc816c
Ville Touronen <touronen@gmail.com>
2020-12-23 20:50:35 +0200

    Set size

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,7 @@ fn main() {
     env_logger::init();
     let event_loop = EventLoop::new();
     let window = WindowBuilder::new()
+        .with_inner_size(winit::dpi::LogicalSize::new(1650, 1080))
         .build(&event_loop)
         .unwrap();
 
