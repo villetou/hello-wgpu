1687012aeee0b27055620c14910a052f6a34281a
Ville Touronen <touronen@gmail.com>
2021-01-14 21:31:05 +0200

    Remove unused CameraController stuff

--- src/camera.rs
+++ src/camera.rs
@@ -25,72 +25,21 @@ impl Camera {
     }
 }
 
-pub struct CameraController {
-    speed: f32,
-    is_up_pressed: bool,
-    is_down_pressed: bool,
-    is_left_pressed: bool,
-    is_right_pressed: bool,
-}
+pub struct CameraController {}
 
 impl CameraController {
-    pub fn new(speed: f32) -> Self {
-        Self {
-            speed,
-            is_up_pressed: false,
-            is_down_pressed: false,
-            is_left_pressed: false,
-            is_right_pressed: false,
+    pub fn update_camera_with_controller(controller: &crate::controller::Controller, camera: &mut Camera) {
+        if controller.is_up_pressed {
+            camera.center.y = camera.center.y + controller.speed;
         }
-    }
-
-    pub fn process_events(&mut self, event: &WindowEvent) -> bool {
-        match event {
-            WindowEvent::KeyboardInput {
-                input: KeyboardInput {
-                    state,
-                    virtual_keycode: Some(keycode),
-                    ..
-                },
-                ..
-            } => {
-                let is_pressed = *state == ElementState::Pressed;
-                match keycode {
-                    VirtualKeyCode::W | VirtualKeyCode::Up => {
-                        self.is_up_pressed = is_pressed;
-                        true
-                    }
-                    VirtualKeyCode::A | VirtualKeyCode::Left => {
-                        self.is_left_pressed = is_pressed;
-                        true
-                    }
-                    VirtualKeyCode::S | VirtualKeyCode::Down => {
-                        self.is_down_pressed = is_pressed;
-                        true
-                    }
-                    VirtualKeyCode::D | VirtualKeyCode::Right => {
-                        self.is_right_pressed = is_pressed;
-                        true
-                    }
-                    _ => false,
-                }
-            }
-            _ => false,
+        if controller.is_down_pressed {
+            camera.center.y = camera.center.y - controller.speed;
         }
-    }
-
-    pub fn update_camera(&mut self, camera: &mut Camera) {
-        if self.is_up_pressed {
-            camera.center.y = camera.center.y + self.speed;
+        if controller.is_right_pressed {
+            camera.center.x = camera.center.x + controller.speed;
         }
-        if self.is_down_pressed {
-            camera.center.y = camera.center.y - self.speed;
-        }
-        if self.is_right_pressed {
-            camera.center.x = camera.center.x + self.speed;
-        }
-        if self.is_left_pressed {
-            camera.center.x = camera.center.x - self.speed;
+        if controller.is_left_pressed {
+            camera.center.x = camera.center.x - controller.speed;
         }
     }
 }
